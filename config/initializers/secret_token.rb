666ff9a0ecd2711e316f38e43d907f77638a37bd
Gesiel Rios <gesielrios@gmail.com>
2011-02-14 20:03:39 -0300

    Comite inicial do Projeto do Curso da Caelum R71 com Alberto.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VotaPrato::Application.config.secret_token = '6cf7e7f37c7baf21e19574dce4436e3d5d13a41cdd8691b6d9620752f7c2df0b81d0dba05c74e2a2418deb2c201615a874e699577998a0bb3cfd4e5680051ca1'

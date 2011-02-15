535442b5d4ebd3a0584090e2d72f0ed41cc58d3f
Gesiel Rios <gesielrios@gmail.com>
2011-02-15 20:34:53 -0300

    Implementacao das validacoes na classe Restaurante, exercicio pag. 67

--- app/models/restaurante.rb
+++ app/models/restaurante.rb
@@ -1,2 +1,19 @@
 class Restaurante < ActiveRecord::Base
+
+  validates_presence_of :nome, :message => "deve ser preenchido"
+  validates_presence_of :endereco, :message => "deve ser preenchido"
+  validates_presence_of :especialidade, :message => "deve ser preenchido"
+
+  validates_uniqueness_of :nome, :message => "nome já cadastrado"
+  validates_uniqueness_of :endereco, :message => "endreço já cadastrado"
+
+  validate :primeira_letra_deve_ser_maiscula
+
+  private
+
+  def primeira_letra_deve_ser_maiscula
+    erros.add("nome", "primeira letra deve ser maiscula") unless nome =~ /[A-Z].*
+  end
+
 end
+

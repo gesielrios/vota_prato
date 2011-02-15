ff235463153d0708a4b945b806e03f0a8b80b75a
Gesiel Rios <gesielrios@gmail.com>
2011-02-14 21:04:48 -0300

    Configuracao do database.yml e exercicio sobre migrations: Criar tabela restaurantes e adicionar especialidade.

---
+++ db/migrate/20110215000002_add_column_especialidade_to_restaurante.rb
@@ -0,0 +1,10 @@
+class AddColumnEspecialidadeToRestaurante < ActiveRecord::Migration
+  def self.up
+    add_column :restaurantes, :especialidade, :string, :limit => 40
+  end
+
+  def self.down
+    remove_column :restaurantes, :especialidade
+  end
+end
+
